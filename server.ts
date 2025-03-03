187737179203ddb86a748edf91bd19bb5f0a98e0
bedrockdude10 <dannyrollo4@gmail.com>
2025-03-03 15:49:38 -0500

    change to use new middleware

--- server.ts
+++ server.ts
@@ -1,14 +1,8 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-
-import * as Home from './pages/home';
-import * as Books from './pages/books';
-import * as BooksStatus from './pages/books_status';
-import * as Authors from './pages/authors';
-import * as BookDetails from './pages/book_details';
-import * as CreateBook from './pages/create_book';
+import router from './pages/router';  // Import the new router
 
 const app = express();
 const port = 8000;
@@ -17,6 +11,7 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+// MongoDB Connection
 const mongoDB = 'mongodb://127.0.0.1:27017/my_library_db';
 mongoose.connect(mongoDB);
 const db = mongoose.connection;
@@ -26,42 +21,10 @@ db.on('connected', () => {
   console.log('Connected to database');
 });
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/home', (_, res: Response) => {
-  Home.show_home(res);
-});
-
-app.get('/available', (_, res: Response) => {
-  BooksStatus.showAllBooksStatus(res);
-});
-
-app.get('/books', async (_, res: Response) => {
-  try {
-    const data = await Books.showBooks();
-    res.send(data);
-  } catch {
-    res.send('No books found');
-  }
-});
-
-app.get('/authors', (_, res: Response) => {
-  Authors.showAllAuthors(res);
-});
-
-app.get('/book_dtls', (req: Request, res: Response) => {
-  BookDetails.showBookDtls(res, req.query.id as string);
-});
-
-app.post('/newbook', (req: Request, res: Response) => {
-  const { familyName, firstName, genreName, bookTitle } = req.body;
-  if (familyName && firstName && genreName && bookTitle) {
-    CreateBook.new_book(res, familyName, firstName, genreName, bookTitle).catch(err => {
-      res.send('Failed to create new book ' + err);
-    });
-  } else {
-    res.send('Invalid Inputs');
-  }
-});
+// Use the new router for all routes
+app.use('/', router);
